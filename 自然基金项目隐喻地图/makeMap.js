9bba46a81c429f64752673e921037d1cf17dd711
Knock <30502484+CodePlayee@users.noreply.github.com>
2018-03-28 10:47:42 +0800

    Update makeMap.js

--- 自然基金项目隐喻地图/makeMap.js
+++ 自然基金项目隐喻地图/makeMap.js
@@ -1,4 +1,4 @@
-//last edited by GaoZhen at 2018/3/21 
+//last edited by GaoZhen at 2018/3/28 
 //all copy right hold by GaoZhen.
 //free to use for only study and research purpose,please contact 1295020109@qq.com for permission on commercial usage.
 
@@ -8,18 +8,24 @@
 // 3.鼠标点击多边形，相应的区域边界颜色改变，标注也有相应变化
 // 4.在多边形上实现自适应标注
 
+//重新组织后的数据
+var departments=new Array();//学部
+var subjects=new Array();//学科
+var sub_disciplines=new Array();//子学科
+var programs=new Array();//（个人项目）
+var all_data=[];//存放所有图层的原始数据
+var groups=[];//存放所有图层
+
 // 说明：我们使用D3虽然导入的是topojson文件，但D3通过将TopoJSON对象转换为GeoJSON再绘制地图，
 // 所以实质还是使用GeoJSON对象绘制地图
 window.onload=function () {
 
     var map_div_width=document.getElementById("map_div").offsetWidth,
         map_div_height=document.getElementById("map_div").offsetHeight;
-
-    var width = 800,
-        height =500,
+    var width =map_div_width,
+        height =map_div_height,
         active = d3.select(null),
-        current_level=1,
-        LOD_valid=true;//放大是否显示细节(只在多边形上点击放大居中时不用)
+        current_level=1;
 
     var projection = d3.geo.mercator()//定义投影
         .scale(140000)//设置缩放量
@@ -32,25 +38,26 @@ window.onload=function () {
     var zoom = d3.behavior.zoom()
         .translate([0,0])
         .scale(1)
-        .scaleExtent([1,12])
-        .on("zoom", zoomed);
+        .scaleExtent([1,20])
+        .on("zoom", zoomed)
+       zoom.LOD_valid=true;//默认放大时，会显示更精细的图层。(只在多边形上点击放大居中时不用)
 
     var svg = d3.select("svg")
-        .attr("width", width)
-        .attr("height", height)
-        .attr("transform", "translate("
-            +(map_div_width-width)/2+","
-            +(map_div_height-height)/2
-            +")")
+        .attr("width",map_div_width) //width
+        .attr("height",map_div_height) //height
+        // .attr("transform", "translate("
+        //     +(map_div_width-width)/2+","
+        //     +(map_div_height-height)/2
+        //     +")")
         .on("click", stopped, true);
 
     svg.append("rect")
         .attr("class", "background")
-        .attr("width", width)
-        .attr("height", height)
-        .on("click", reset);//clicked
+        .attr("width", map_div_width)
+        .attr("height", map_div_height)
+        .on("click", reset);
 
-    var g = svg.append("g");//地图图层所在的总group
+    var g = svg.append("g")
     var g1 = g.append("g");//第一层
     g1.level=1;
     var g2 = g.append("g");//第二层
@@ -60,72 +67,113 @@ window.onload=function () {
     var g4 = g.append("g");//第四层
     g4.level=4;
     var g5 = g.append("g");//第五层
-    g4.level=5;
+    g5.level=5;
+    groups=[g1,g2,g3,g4,g5];
 
     svg.call(zoom) // delete this line to disable free zooming
         .call(zoom.event);
 
-    createMap("http://localhost:8188/data/first2016Z_topo.json",g1);
-    createMap("http://localhost:8188/data/second2016Z_topo.json",g2);
-    createMap("http://localhost:8188/data/third2016Z_topo.json",g3);
-    createMap("http://localhost:8188/data/fourth2016Z_topo.json",g4);
-    createMap("http://localhost:8188/data/fifth2016Z_topo.json",g5);
-    g1.style("display","inline");
+    var urls=["http://localhost:8188/data/first2016Z_topo.json",
+        "http://localhost:8188/data/second2016Z_topo.json",
+        "http://localhost:8188/data/third2016Z_topo.json",
+        "http://localhost:8188/data/fourth2016Z_topo.json",
+        "http://localhost:8188/data/fifth2016Z_topo.json"];
+
+    //再使用基于多个d3.json读取的数据之前，使用queue.js加载完这些数据再做下一步处理
+    queue()
+        .defer(d3.json,urls[0])
+        .defer(d3.json,urls[1])
+        .defer(d3.json,urls[2])
+        .defer(d3.json,urls[3])
+        .defer(d3.json,urls[4])
+        .await(createMap);
+
+    window.data_over=true;
+    for(var i=1;i<groups.length;i++){
+        groups[i].style("display","none");
+    }
+    //页面加载后执行到上行。。。。。。。。。。。。。。。。。。。。。。。。。。。。
+
+
 
     //缩放按钮 button
     d3.selectAll('.zoom_bt').on('click', zoomClick);
     //复位按钮
     d3.selectAll('#reset').on('click', function () {
-        svg.transition()
-            .duration(750)
-            .call(zoom.translate([0, 0]).scale(1).event);
-        LOD_valid=true;
+        reset();
+        zoom.LOD_valid=true;
     });
+    //数据重组织后查看相关统计信息
+    d3.select("#reorganize").on('click',function () {
+        //alert(g2.style("display"));
+        var min= processedShow([g1,g2,g3,g4,g5]);
+        var msg=" ";
+        min.forEach(function (d) { msg+=d+"</br>"; })
+        document.getElementById("statistic").innerHTML=msg;
+    })
 
-    //鼠标在多边形上的点击事件
-    function clicked(d)
-    {
-        LOD_valid=false;
 
-        //d为鼠标点击所属的多边形对象，有geometry 和properties属性
-        //显示多边形的详细信息
-        var message="<b>detail:</b>"+"<br/>";
-        for(var item in d.properties)
+    //鼠标在多边形上的点击事件(mouseup)
+    //d和d3中的data有关，而this是按下鼠标时光标所在的dom元素
+    function focus(d,current_this) {
         {
-            if(d.properties[item])
-            message +=item+": "+d.properties[item]+"<br/>";
+            zoom.LOD_valid=false;
+            var all_layers=[departments,subjects,sub_disciplines,programs];
+            var groups=[g1,g2,g3,g4,g5]
+            var which_layer=whichLayer(groups);
+            var current_layer=all_layers[which_layer-1]; //g1不算
+            groups=null;
+
+
+            //d为鼠标点击所属的多边形对象，有geometry 和properties属性
+            //显示多边形的详细信息
+            var message="<b>detail:</b>"+"<br/>";
+            for(var item in d.properties)
+            {
+                if(d.properties[item])
+                    message +=item+": "+d.properties[item]+"<br/>";
+            }
+            document.getElementById("detail").innerHTML=message;
+
+            if (active.node() === current_this){
+             //   active.style("fill",d.fillColor);
+                return reset();//在已激活的多边形上再次点击,则恢复到初始状态
+            }
+
+            //将上一步激活的多边形恢复
+            if(active.node()!=null){
+                active.style("fill",active.node().__data__.fillColor);
+                active.classed("active", false);
+            }
+
+            active = d3.select(current_this).classed("active", true);
+            active.style("fill","#ff4433"); //鼠标点击区域变色
+
+            //放大与对焦
+            var bounds = path.bounds(d),
+                dx = bounds[1][0] - bounds[0][0],
+                dy = bounds[1][1] - bounds[0][1],
+                x = (bounds[0][0] + bounds[1][0]) / 2,
+                y = (bounds[0][1] + bounds[1][1]) / 2,
+                // scale = Math.max(1, Math.min(8, 0.9 / Math.max(dx / width, dy / height))),
+                scale = Math.max(1, Math.min(8, 0.9 / Math.max(dx / (width*0.7), dy / (height*0.7) ))),//乘以0.7是防止放大得过大
+                translate = [width / 2 - scale * x, height / 2 - scale * y];
+
+            svg.transition()
+                .duration(750)  //750
+                .call(zoom.translate(translate).scale(scale).event);
+
         }
-        document.getElementById("detail").innerHTML=message;
-
-        if (active.node() === this)
-            return reset();//在已激活的多边形上再次点击,则恢复到初始状态
-        active.classed("active", false);//将上一步激活的多边形恢复
-        active = d3.select(this).classed("active", true);
-
-        //对焦
-        var bounds = path.bounds(d),
-            dx = bounds[1][0] - bounds[0][0],
-            dy = bounds[1][1] - bounds[0][1],
-            x = (bounds[0][0] + bounds[1][0]) / 2,
-            y = (bounds[0][1] + bounds[1][1]) / 2,
-            scale = Math.max(1, Math.min(8, 0.9 / Math.max(dx / width, dy / height))),
-            translate = [width / 2 - scale * x, height / 2 - scale * y];
-
-        svg.transition()
-            .duration(750)
-            .call(zoom.translate(translate).scale(scale).event);
-
     }
-
 
 
     //缩放函数
     function zoomed() {
-        g.style("stroke-width", 1.5 / d3.event.scale + "px");
+        g.style("stroke-width",1.5 / d3.event.scale.toFixed(2) + "px" );  //
         g.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
-        document.getElementById("show_scale").innerHTML="<b>current scale:</b> "+zoom.scale().toFixed(2);
+        document.getElementById("show_scale").innerHTML="<b>current scale:</b> "+zoom.scale().toFixed(2)+zoom.LOD_valid;
 
-        LOD(zoom.scale(),LOD_valid);
+        LOD(zoom.scale(),zoom.LOD_valid);
     }
 
 
@@ -136,8 +184,9 @@ window.onload=function () {
     }
 
 
-//缩放按钮点击事件
+    //缩放按钮点击事件
     function zoomClick() {
+        zoom.LOD_valid=true;
         var clicked = d3.event.target,
             direction = 1,
             factor = 0.2,//缩放按钮点击一次增大或减少的倍数
@@ -175,27 +224,24 @@ window.onload=function () {
                     .translate(iTranslate(t));//相应的zoom属性也要变化
 
                 //此处没有调用zoomed函数
-                g.style("stroke-width", 1.5 / iScale(t) + "px");
+                g.style("stroke-width",1.5 / iScale(t) + "px");
                 g.attr("transform", "translate(" + iTranslate(t) + ")scale(" + iScale(t) + ")");
 
                 var show_scale=document.getElementById("show_scale").innerHTML="current scale: "+zoom.scale().toFixed(2);
-                LOD_valid=true;
-                LOD(zoom.scale(), LOD_valid);
+
+                LOD(zoom.scale(), zoom.LOD_valid);
             };
         });
     }
 
-//读取json数据并在特定<g>标签里显示图形
-    function createMap(url,group) {
-        d3.json( url,
-            function(error, root)
-            {
-             if (error) throw error;
-
-            //读取成功
-            //将topoJSON对象(root)转换为GeoJSON，保存在geoRoot中
-            //然而需要注意的是，实际上在绘制地图时，还是使用了GeoJSON对象(坐标为经纬度)。
-            //feature方法返回GeoJSON的要素（Feature）或要素集合（FeatureColleciton）
+    //读取数据，绘制地图
+    function createMap(err,root0,root1,root2,root3,root4) {
+        if (err) throw err;
+        var roots=[root0,root1,root2,root3,root4];
+        for(var i=0;i<roots.length;i++){
+            var group=groups[i];
+            var root=roots[i];
+            //将topoJSON对象(root)转换为GeoJSON，保存在geoRoot中.  在绘制地图时，还是使用了GeoJSON对象(坐标为经纬度)。
             var geoRoot = topojson.feature(root,root.objects.collection);//整个geoJson对象，数据在geoRoot.features
 
             //获取地图范围
@@ -209,107 +255,106 @@ window.onload=function () {
                 .append("path")
                 .attr("d", path)
                 .attr("class",function (d) {
+                    var currentClass;
                     switch(group.level)
                     {
                         case 1:
-                            return "subunit "+"firstLevel";
+                            currentClass="subunit "+"firstLevel";
+                            break;
                         case 2:
-                            return "subunit "+d.properties.name;
+                            currentClass="subunit "+d.properties.name;
+                            break;
                         case 3:
-                            return "subunit "+d.properties.parentname;
+                            currentClass="subunit "+d.properties.parentname;
+                            break;
                         default:
-                            return "the_last_2_layers";  //"subunit "+d.properties.parentname;
+                            currentClass="subunit "+"the_last_2_layers";
                     }
-                });
-                // .on("click", clicked);
+
+                    d.fillColor=polygonFillColor(currentClass);
+                    d3.select(this).style("fill",d.fillColor);
+
+                   // all_data.push(d);
+                     reorganize(d);//对原数据进行重组织。
+                    return currentClass;
+                })
+                .on("mouseover",function () {
+                    d3.select(this).style("opacity","0.7");
+                    d3.select(this).style("stroke","#fff");
+                })
+                .on("mouseout",function () {
+                    d3.select(this).style("opacity","1");//"fill",d.fillColor
+                    d3.select(this).style("stroke","#777");
+                })
+                .on("mouseup", function (d) {  //click
+                    focus(d,this);//d和d3中的data有关，而this是按下鼠标时其所在的dom元素
+                })
+                .append("title")  // 添加tooltip（即鼠标悬浮停留一会，就会显示相关信息）
+                .text(function(d){return d.properties.name});
 
 
-            //内部边界
-            //     group.append("path")
-            //         //datum 单数
-            //     // by filtering on a === b or a !== b, we obtain exterior or interior boundaries exclusively.
-            //     .datum(topojson.mesh(root,  root.objects.collection, function(a, b) { return a !== b; }))
-            //     .attr("class", "polygon-borders")
-            //     .attr("d", path);
+            //各个多边形内部边界,不包括整体的外部轮廓
+            //datum 单数
+            // by filtering on a === b or a !== b, we obtain exterior or interior boundaries exclusively.
+            group.append("path")
+                .datum(topojson.mesh(root,  root.objects.collection, function(a, b) { return a !== b; }))
+                .attr("class", "polygon-borders")
+                .attr("d", path);
 
-                //外部边界
-                group.append("path")
-                    .datum(topojson.mesh(root,  root.objects.collection, function(a, b) { return a === b; }))
-                    .attr("class", "out_line")
-                    .attr("d", path);
+            //外部边界
+            group.append("path")
+                .datum(topojson.mesh(root,  root.objects.collection, function(a, b) { return a === b; }))
+                .attr("class", "out_line")
+                .attr("d", path);
 
-               //鼠标悬浮作用层
-                group.append("g")
-                    .attr("class","subjects")  //这个类属性是加给 g 的
-                    .selectAll("path")
-                    .data(geoRoot.features)
-                    .enter()
-                    .append("path")
-                    .attr("d", path)
-                    .attr("id", function (d) {   //某个level下的整体图层（包括各个多边形）
-                        return "level"+d.properties.depth;
-                    })
-                    .on("click", clicked)
-					.append("title")  // 添加tooltip（即鼠标悬浮停留一会，就会显示相关信息）
-				    .text(function(d){return d.properties.name});
-
-                //显示多边形内的标注
-                showPolygonLabel(group,geoRoot.features,projection);
-            });
-        
-        group.style("display","none");//只绘制，不显示
-        return group;
+            //显示多边形内的标注
+            showPolygonLabel(group,geoRoot.features,projection);
+           // group.style("display","none");//只绘制，不显示
+        }
     }
+
 
 //根据缩放倍数显示不同细节图层
 function LOD(scale,valid)
 {
+    var layers=[g1,g2,g3,g3,g4,g5];
+    var disappear=function () {
+        for(var i=0;i<layers.length;i++){
+            layers[i].style("display","none");
+        }
+    }
+
     if(valid)
     { var scale=scale.toFixed(1);
         if(scale-1.2<=0.1  && scale-1.2>=-0.1)  //临界点1.2    范围 1.1-1.3
         {
-            g2.style("display","none");
-            g3.style("display","none");
-            g4.style("display","none");
-            g5.style("display","none");
-            g1.style("display","inline");
+            disappear();
+            g1.style("display","");
             current_level=1;
         }
-       // if(scale-1.4<=0.1 && scale-1.4>=-0.1 || scale-1.6<=0.1 && scale-1.6>=-0.1)   //临界点1.4 and 1.6
+
         if(scale-1.5<=0.2 && scale-1.5>=-0.2)   //范围 1.3-1.7
         {
-            g1.style("display","none");
-            g3.style("display","none");
-            g4.style("display","none");
-            g5.style("display","none");
-            g2.style("display","inline");
+            disappear();
+            g2.style("display","");
             current_level=2;
         }
         if(scale-2.0<=0.7 && scale-2.0>=-0.3)   // 1.7-2.7
         {
-            g1.style("display","none");
-            g2.style("display","none");
-            g4.style("display","none");
-            g5.style("display","none");
-            g3.style("display","inline");
+            disappear();
+            g3.style("display","");
             current_level=3;
         }
-        if(scale-3.0<=0.7 && scale-3.0>=-0.3)      // 2.7-3.7
+        if(scale-3.0<=2.2 && scale-3.0>=-0.3)      // 2.7-5.2
         {
-            g1.style("display","none");
-            g2.style("display","none");
-            g3.style("display","none");
-            g5.style("display","none");
-            g4.style("display","inline");
+            disappear();
+            g4.style("display","");
             current_level=4;
         }
-        if(scale>3.7)
+        if(scale>5.2)
         {
-            g1.style("display","none");
-            g2.style("display","none");
-            g3.style("display","none");
-            g4.style("display","none");
-            g5.style("display","inline");
+            disappear();
+            g5.style("display","");
             current_level=5;
         }
 
@@ -319,7 +364,21 @@ function LOD(scale,valid)
 
 }
 
+//鼠标点击已激活多边形区域或者背景矩形区域，恢复到初始状态
+    function reset()
+    {
+        if(active.node()!=null){
+            active.style("fill",active.node().__data__.fillColor);
+            active.classed("active", false);
+        }
 
+        active = d3.select(null);
+        svg.transition()
+            .duration(750)
+            .call(zoom.translate([0, 0]).scale(1).event);
+
+        document.getElementById("detail").innerHTML="<b>detail:</b> "+"<br/>"+ "2016年度国家自然基金资助项目统计详情";
+    }
 
 
 
@@ -327,54 +386,9 @@ function LOD(scale,valid)
 }
 
 
-//
-//鼠标点击已激活多边形区域或者背景矩形区域，恢复到初始状态
-function reset()
-{// LOD_valid=true;
-    active.classed("active", false);
-    active = d3.select(null);
-    svg.transition()
-        .duration(750)
-        .call(zoom.translate([0, 0]).scale(1).event);
 
-    document.getElementById("detail").innerHTML="<b>detail:</b> "+"<br/>"+ "2016年度国家自然基金资助项目统计详情";
-}
 
-//在多边形内部标注文字 group代表g标签，features代表要素集，projection代表投影
-function showPolygonLabel(group,features,projection)
-{
-    var font_size=8;
-    switch (group.level){
-        case 1:
-            font_size=15;
-            break;
-        case 2:
-            font_size=10;
-            break;
-        case 3:
-            font_size=5;
-            break;
-        default:
-            font_size=2.5;
-            break;
-    }
 
-    group.selectAll("text")
-        .data(features)
-        .enter()
-        .append("text")
-        .attr("x",function (d) {
-            var gravityCenter=getGravityCenter(d.geometry.coordinates[0]);//多边形重心，coordinates里的元素是数组
-            var projected_zx=projection([gravityCenter.x,gravityCenter.y]);//投影后的重心，返回的是数组[ , ]
-            return  projected_zx[0];
-        })
-        .attr("y",function (d) {
-            var gravityCenter=getGravityCenter(d.geometry.coordinates[0]);
-            var projected_zx=projection([gravityCenter.x,gravityCenter.y]);
-            return  projected_zx[1];
-        })
-        .attr("class","level"+group.level+"label")
-        .style("text-anchor","middle")
-        .style("font-size",font_size)
-        .text(function (d) { return d.properties.name; });
-}
+
+
+
